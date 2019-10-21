66b08e19b7f464149541ca71abc16f2a64a51e3e
jean <jeanjoseph19@gmail.com>
2019-10-21 12:46:44 +0530

    first commit

---
+++ src/main/java/org/manga/configanalyzer/ConfiganalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.manga.configanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiganalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiganalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/manga/configanalyzer/ConfiganalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manga.configanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiganalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
